16bcb21a12e625cab8d0c43f9cca9a246c5161d5
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2025-12-03 15:57:44 +0100

    reconstruction

--- Assets/Scripts/Character/Player/Arrow/ArrowScript.cs
+++ Assets/Scripts/Character/Player/Arrow/ArrowScript.cs
@@ -2,39 +2,38 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour
 {
-    [SerializeField] private LayerMask enemyLayer;
+    // [SerializeField] private LayerMask enemyLayer;
 
-    private Rigidbody boxRb;
-    private BoxCollider boxCollider;
+    // private Rigidbody boxRb;
+    // private BoxCollider boxCollider;
 
     void Awake()
     {
-        boxRb = gameObject.GetComponent<Rigidbody>();
-        boxCollider = gameObject.GetComponent<BoxCollider>();
+        // boxRb = gameObject.GetComponent<Rigidbody>();
+        // boxCollider = gameObject.GetComponent<BoxCollider>();
     }
 
-    private void StickyOnTarget(Transform target)
-    {
-        boxRb.linearVelocity = Vector3.zero;
-        boxRb.angularVelocity = Vector3.zero;
+    // private void StickyOnTarget(Transform target)
+    // {
+    //     boxRb.linearVelocity = Vector3.zero;
+    //     boxRb.angularVelocity = Vector3.zero;
         
-        transform.SetParent(target);
-        Debug.Log("Flèche plantée dans l'ennemi !");
-    }
+    //     transform.SetParent(target);
+    //     Debug.Log("Flèche plantée dans l'ennemi !");
+    // }
 
-    private void UnabledRigidAndBox()
-    {
-        boxRb.isKinematic = true;
-        boxCollider.isTrigger = true;
-        // boxCollider.enabled = false;
-    }
+    // private void UnabledRigidAndBox()
+    // {
+    //     boxRb.isKinematic = true;
+    //     boxCollider.isTrigger = true;
+    //     // boxCollider.enabled = false;
+    // }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(((1 << collision.gameObject.layer) & enemyLayer.value) != 0)
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ennemy"))
         {
-            StickyOnTarget(collision.transform);
-            UnabledRigidAndBox();
+            Destroy(gameObject);
         }
     }
 }
